10538d1f6e45c9dc5e2b0bd148f83c5d7c0b7cd8
Anna Little Bird <55469854+Anna-Little-Bird@users.noreply.github.com>
2020-03-19 20:51:18 +1300

    Added files
    Slowly but surely making my way...

--- code/src/TrayCalendar.java
+++ code/src/TrayCalendar.java
@@ -3,73 +3,26 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TrayCalendar
-//implements ActionListener
 {
-	public static void main(String[] args) {
-//		Number j;
-//		Integer k;
-		int i;
-		String day;
-		JFrame frame = new JFrame("Calendar");
-		//frame.add(new HelloComponent("Hello, Java!"));
-		for (i=1; i<32; i++) {
-			day=Integer.toString(i);
-			frame.add(new JButton(day));
-		}
-//		frame.add(new JButton("1"));
-//		frame.add(new JButton("2"));
-		frame.setLayout(new GridLayout(0,7));
+	public static void main(String[] args)	// main method
+	{
+		JFrame frame = new JFrame("Calendar");	// creating Calendar's main window
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(350, 350);
+		frame.setSize(150, 150);
 		frame.setVisible(true);
+		JButton Button1=new JButton("1");
+		frame.add(Button1);
+		frame.setLayout(new FlowLayout());
+		Button1.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource()== Button1) {
+					JFrame frame2;
+					frame2=new JFrame("1");
+					frame2.setSize(150, 150);
+					frame2.setVisible(true);
+				}	
+			}		
+		});
 	}
 }
-
-/*class HelloComponent extends JComponent
-	implements MouseMotionListener, ActionListener
-{
-	String theMessage;
-	int messageX=125, messageY=95;
-
-	JButton theButton;
-	
-	int colorIndex;
-	static Color[] someColors={Color.BLACK,Color.red,Color.GREEN,Color.blue,Color.magenta};
-	
-	public HelloComponent(String message) {
-		theMessage=message;
-		theButton=new JButton("Change Color");
-		setLayout(new FlowLayout());
-		add(theButton);
-		theButton.addActionListener(this);
-		addMouseMotionListener(this);
-	}
-	
-	public void paintComponent(Graphics g) {
-		g.drawString(theMessage, messageX, messageY);
-	}
-	
-	public void mouseDragged(MouseEvent e) {
-		messageX=e.getX();
-		messageY=e.getY();
-		repaint();
-	}
-	
-	public void mouseMoved(MouseEvent e) {}
-	
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource()==theButton)
-			changeColor();
-	}
-	
-	synchronized private void changeColor() {
-		if (++colorIndex==someColors.length)
-			colorIndex=0;
-		setForeground(currentColor());
-		repaint();
-	}
-	
-	synchronized private Color currentColor() {
-		return someColors[colorIndex];
-	}
-}*/
