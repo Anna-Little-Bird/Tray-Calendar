311d8697169f41d10710914292c4a7885fc046a9
Anna Little Bird <55469854+Anna-Little-Bird@users.noreply.github.com>
2020-03-30 21:15:17 +1300

    Updated program files
    Making some basic components (button, new window, text area and fields, labels) work.

--- code/src/TrayCalendar.java
+++ code/src/TrayCalendar.java
@@ -7,22 +7,48 @@ public class TrayCalendar
 	public static void main(String[] args)	// main method
 	{
 		JFrame frame = new JFrame("Calendar");	// creating Calendar's main window
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(150, 150);
-		frame.setVisible(true);
-		JButton Button1=new JButton("1");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// setting the CLOSE button to clote the program
+		frame.setSize(200, 200);				// window size
+		JButton Button1=new JButton("1");		// adding the Button
 		frame.add(Button1);
-		frame.setLayout(new FlowLayout());
-		Button1.addActionListener(new ActionListener()
+		frame.setLayout(new FlowLayout());		// might change to GridLayout?
+		frame.setVisible(true);
+		Button1.addActionListener(new ActionListener()		// adding some interaction to the Button 
 		{
 			public void actionPerformed(ActionEvent e) {
-				if (e.getSource()== Button1) {
-					JFrame frame2;
-					frame2=new JFrame("1");
-					frame2.setSize(150, 150);
-					frame2.setVisible(true);
+				if (e.getSource()== Button1) {				// when the Button is clicked
+					frame.add(new DayTask());				// open a new window
 				}	
 			}		
 		});
+	}
+}
+
+class DayTask extends JComponent
+{
+	JFrame task;
+	JTextField taskName;
+	JTextArea taskText;
+	JLabel labelName,labelText;
+	
+	public DayTask() {					// playing with the window appearance
+		task=new JFrame("1");
+		task.setSize(350, 150);
+		taskName=new JTextField("");
+		taskText=new JTextArea("");
+		labelName=new JLabel("Name:");
+		labelText=new JLabel("<html>Task<br>Description:</html>");
+		labelText.setVerticalAlignment(JLabel.TOP);
+		task.add(taskName);
+		task.add(taskText);
+		task.add(labelName);
+		task.add(labelText);
+		task.setLayout(null);
+		Insets insets = task.getInsets();
+		labelName.setBounds(25+insets.left,5+insets.top,50,20);
+		taskName.setBounds(100+insets.left,5+insets.top,100,20);
+		labelText.setBounds(25+insets.left,55+insets.top,70,50);
+		taskText.setBounds(100+insets.left,55+insets.top,70,50);
+		task.setVisible(true);
 	}
 }
